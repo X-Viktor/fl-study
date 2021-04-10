76ae546da95c8388d0e17261e2b91cd329ea0532
X_Viktor <viktor@MacBook-Pro-Viktor.local>
2021-04-10 16:41:03 +0700

    Add categories

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import OrderListView
+from .views import OrderListView, OrderListByCategoryView
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='main'),
+    path('category/<slug:slug>/', OrderListByCategoryView.as_view(), name='category')
 ]
--- orders/views.py
+++ orders/views.py
@@ -4,9 +4,20 @@ from .models import Order
 
 
 class OrderListView(ListView):
+    """ Вывод всех заказов """
     model = Order
     template_name = 'orders/main.html'
     context_object_name = 'orders'
 
     def get_queryset(self):
         return Order.objects.filter(stage=1).select_related('category')
+
+
+class OrderListByCategoryView(ListView):
+    """ Вывод заказов определенной категории """
+    model = Order
+    template_name = 'orders/main.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return Order.objects.filter(category__slug=self.kwargs['slug'], stage=1).select_related('category')
